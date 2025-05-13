0257f059ffee14253ff8575d83bb459697def988
ziatzkia <zia.tazkiatul@mhs.itenas.ac.id>
2025-05-13 10:21:39 +0700

    dashboar user , navbar, footer

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-dark py-3" style="background-color: #1f2f23; position: relative;">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+        <!-- Left Side -->
+        <div class="d-flex align-items-center">
+            <a class="nav-link text-white me-3" href="#">Discover</a>
+            <a class="nav-link text-white me-3" href="#">Shop</a>
+            <a class="nav-link text-white" href="#">Catalogue</a>
+        </div>
+
+        <!-- Logo -->
+        <a class="navbar-brand position-absolute top-50 start-50 translate-middle text-white text-center" href="#" style="font-weight: bold;">
+            <small style="font-size: 10px;">MAISON</small><br />
+            <span style="font-size: 24px;">SONO</span>
+        </a>
+
+        <!-- Right Side -->
+        <div class="d-flex align-items-center">
+            <ul class="navbar-nav d-flex flex-row align-items-center mb-0">
+                <li class="nav-item me-3">
+                    <a href="#" class="nav-link text-white">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </li>
+                <li class="nav-item me-3">
+                    <a href="#" class="nav-link text-white">
+                        <i class="bi bi-person"></i>
+                    </a>
+                </li>
+                <li class="nav-item me-3">
+                    <a href="#" class="nav-link position-relative text-white">
+                        <i class="bi bi-bag"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</head>
+
+<body>
+     @yield('content')
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-7+Q1j6g2m4v8e0c9xk+6j5g5f5e5f5e5f5e5f5e5f5e5f5e5f5e5f5e" crossorigin="anonymous"></script> --}}
+</body>
+
+</html>
